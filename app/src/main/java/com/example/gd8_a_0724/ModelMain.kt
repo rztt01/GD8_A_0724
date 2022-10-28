095186f44d43d34c0bfd44fbb4dd716f3c2e0d93
rztt01* <vinsariest@gmail.com>
2022-10-28 21:33:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd8_a_0724/ModelMain.kt
@@ -0,0 +1,11 @@
+package com.example.gd8_a_0724
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModelMain {
+    lateinit var strName: String
+    lateinit var strVicinity: String
+    var latloc = 0.0
+    var longloc = 0.0
+}
